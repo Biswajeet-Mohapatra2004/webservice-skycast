f120baddc8fae954538b4e3ff744329217d66a32
Biswajeet Mohapatra <mbiswajeet66@gmail.com>
2025-01-31 15:24:10 +0530

    fixed cors

--- src/main/java/com/weather/weather_api/controller/WeatherController.java
+++ src/main/java/com/weather/weather_api/controller/WeatherController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
-@CrossOrigin("https://skycast-mu.vercel.app")
+@CrossOrigin("https://skycast-theta.vercel.app")
 @RestController
 public class WeatherController {
 
